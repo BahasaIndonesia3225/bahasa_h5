2452fd4cbdc0f5be9ff7b702a4d69b7d50b0b621
liyujian3225 <icanflyanywhere@126.com>
2025-04-24 22:30:41 +0800

    通知公告栏功能对接开发

--- src/global.js
+++ src/global.js
@@ -6,13 +6,14 @@ if (isMobileOnly) {
 }
 if (isTablet) {
   // 平板(iPad, 安卓平板...)
-  window.location.href = 'http://studypc.bahasaindo.cn/';
+  // window.location.href = 'http://studypc.bahasaindo.cn/';
+  window.location.href = 'http://studypc.bahasaindo.net';
 }
 if (isMobile) {
   // 移动设备(手机, 平板...)
 }
 if (isDesktop) {
   // 桌面端浏览器
-  window.location.href = 'http://studypc.bahasaindo.cn/';
-  // window.location.href = 'http://studypc.bahasaindo.net';
+  // window.location.href = 'http://studypc.bahasaindo.cn/';
+  window.location.href = 'http://studypc.bahasaindo.net';
 }
