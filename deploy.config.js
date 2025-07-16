ec6be871c7f3a2974b35955c17891e41da0228ee
李雨健 <icanflyanywhere@126.com>
2025-07-16 15:13:55 +0800

    部署更新服务器配置文件数据抽取成独立文件

--- deploy.config.js
+++ deploy.config.js
@@ -1,50 +1,24 @@
+const { serverConfig } = require('./server.config');
 const SftpClient = require('ssh2-sftp-client');
 const notifier = require('node-notifier');
 var dayjs = require('dayjs');
 const sftp = new SftpClient()
 
-const ENV_CONFIG_MAP = {
-  Hongkong: {
-    host: '34.150.122.99',
-    port: '22',
-    username: 'root',
-    password: 'vC60o5MPysDprqFpTJ2g',
-    outputPath: 'bahasaindo.net',
-  },
-  Mainland: {
-    host: '47.115.219.240',
-    port: '22',
-    username: 'root',
-    password: 'Bahasa272727',
-    outputPath: 'study.bahasaindo.cn',
-  },
-  Taiwan: {
-    host: '104.155.209.57',
-    port: '22',
-    username: 'root',
-    password: 'Bahasa272727',
-    outputPath: 'www.bahasaindo.com',
-  }
-};
-
 const envName = process.argv[2];
-const config = ENV_CONFIG_MAP[envName];
-const { outputPath } = config;
+const config = serverConfig[envName];
+const { outputPath, serverName } = config;
+
 sftp.connect(config).then(() => {
   sftp.uploadDir(outputPath, `/www/wwwroot/${outputPath}`).then(() => {
     const time = dayjs().format('YYYY-MM-DD HH:mm:ss')
     notifier.notify({
-      title: '部署成功',
-      message: `部署成功于${time}`
+      title: '部署提示',
+      message: `${serverName}部署成功于${time}`
     });
     return sftp.end()
   }).catch((err) => {
-    if (sftp.sftp) {
-      sftp.sftp.end()
-    }
+    if (sftp.sftp) sftp.sftp.end()
   })
 }).catch((err) => {
-  if (sftp.sftp) {
-    sftp.sftp.end()
-  }
+  if (sftp.sftp) sftp.sftp.end()
 })
---
+++ server.config.js
@@ -0,0 +1,28 @@
+const serverConfig = {
+  Hongkong: {
+    host: '34.150.122.99',
+    port: '22',
+    username: 'root',
+    password: 'vC60o5MPysDprqFpTJ2g',
+    outputPath: 'bahasaindo.net',
+    serverName: '香港线路'
+  },
+  Mainland: {
+    host: '47.115.219.240',
+    port: '22',
+    username: 'root',
+    password: 'Bahasa272727',
+    outputPath: 'study.bahasaindo.cn',
+    serverName: '大陆线路'
+  },
+  Taiwan: {
+    host: '104.155.209.57',
+    port: '22',
+    username: 'root',
+    password: 'Bahasa272727',
+    outputPath: 'www.bahasaindo.com',
+    serverName: '台湾线路'
+  }
+}
+
+module.exports = { serverConfig };
